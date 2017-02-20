da6831f05e74eccf317bddc1521424a7d07f0198
Lee hyunjoo <siwabada@gmail.com>
2017-02-20 19:06:23 +0900

    실습내용 추가

--- opentutorials/app.js
+++ opentutorials/app.js
@@ -1,8 +1,43 @@
+// 테스트에욤
+
 var express = require('express');
-var app = express();
-app.locals.pretty = true;
-app.set('view engine', 'jade')
+var app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'));
+app.locals.pretty = true;
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+app.get('/topic/:id/:mode', function(req, res){
+// 라우터 경로의 변경
+	var topic = [
+		'javascript is...',
+		'nodejs is...',
+		'express is...'
+	];
+	var li = `
+	<li><a href="/topic/0">js</a></li>
+	<li><a href="/topic/1">nodejs</a></li>
+	<li><a href="/topic/2">express</a></li>
+	`
+	res.send(li + '<br>' + topic[req.params.id]  + req.params.mode);
+	//path 방식을 사용하는 url의 경우 params를 통해서 값을 가져올 수 있음
+})
+
+app.get('/form',function(req, res){
+	res.render('form');
+});
+app.get('/form_receiver', function(req, res){
+	var title = req.query.title;
+	var desc = req.query.desc;
+	res.send(title+', 그리고'+desc);
+});
+app.post('/form_receiver', function(req, res) {
+	var t = req.body.title;
+	var d = req.body.desc;
+	res.send(t+','+d);
+});
 app.get('/template', function(req, res){
 	res.render('temp', {time: Date(), _title: 'Jade'});
 }); // 템플릿 엔진의 소스코드를 가지고 와서 웹페이지를 만들어 내는 res 객체의 메소드
---
+++ opentutorials/app_file.js
@@ -0,0 +1,70 @@
+var express = require('express'); // express모듈 적용
+var app = express();
+var fs = require('fs'); // file system nodejs 모듈 적용
+var bodyParser = require('body-parser'); // body-parser 미들웨어 설정
+app.use(bodyParser.urlencoded({ extended: false }));
+app.locals.pretty = true;
+app.set('view engine', 'jade'); // 템플릿 엔진 설정
+app.set('views', './views_file');
+
+//////////////// 라우터 시작 ////////////////
+
+// 작성 - 글 작성후 post로 전송
+app.get('/topic/new', function(req, res){
+  fs.readdir('data',function(err, files) {
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }else{
+      res.render('new',{topics: files});
+    }
+  });
+});
+
+// 저장 - post로 전송된 글 저장
+app.post('/topic', function(req, res) {
+
+  var title = req.body.title;
+  var description = req.body.description;
+  fs.writeFile('data/'+title, description, function(err){
+    if(err){
+      console.log(err);
+      // 에러정보가 터미널에 표시된다.
+      res.status(500).send('Internal Server Error');
+      // send 메소드는, send가 실행되면 그 다음코드는 실행되지 않는다.
+    }
+    res.redirect('/topic/'+title);
+    // callback 함수가 실행된 후에 response가 가능하다.
+  });
+});
+
+// 조회 - 글목록 표시, 글내용
+app.get(['/topic', '/topic/:id'], function(req, res) {
+  fs.readdir('data',function(err, files) {
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    var id = req.params.id;
+    if(id){
+      // id 값이 있을 떄
+      fs.readFile('data/'+id, 'utf8', function(err, data){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        }
+        res.render('view', {topics: files, title: id, description: data});
+      });
+    }else{
+      // id 값이 없을 때
+      res.render('view', {topics:files, title:'welcome', description:'hello JS for server'});
+    }
+  });
+});
+
+//////////////// 라우터 종료 ////////////////
+
+// express 서버를 특정 포트에 연결
+app.listen(3000, function() {
+  console.log('connected, 3000 port!');
+});
